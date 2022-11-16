8eb163dcdb8a7b90863276ebd120f0aab06cbf3d
aibenStunner <ebenezergadri99@gmail.com>
2022-11-16 21:01:06 +0000

    feat: added new note form functionality

--- src/NewNote.tsx
+++ src/NewNote.tsx
@@ -1,10 +1,25 @@
 import { NoteForm } from "./NoteForm";
 
+export type Note = {
+  id: string;
+} & NoteData;
+
+export type NoteData = {
+  title: string;
+  markdown: string;
+  tags: Tag[];
+};
+
+export type Tag = {
+  id: string;
+  label: string;
+};
+
 export function NewNote() {
   return (
     <>
       <h1 className="mb-4">New Note</h1>
-      <NoteForm />
+      <NoteForm onSubmit={() => {}} />
     </>
   );
 }
